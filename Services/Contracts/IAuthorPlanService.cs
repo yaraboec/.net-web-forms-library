2e63be1c25c7a9158ed3806b6487c8ae9026b155
yaroslav.shakalov <yaroslavshakalov888@gmail.com>
2021-12-23 14:21:52 +0200

    New repositories cover by unit tests

---
+++ Services/Contracts/IAuthorPlanService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Entities;
+
+namespace Services.Contracts
+{
+    public interface IAuthorPlanService
+    {
+        AuthorPlan GetById(Guid id);
+
+        IEnumerable<AuthorPlan> GetAll();
+
+        AuthorPlan Create(AuthorPlan authorPlan);
+
+        AuthorPlan Delete(Guid id);
+
+        AuthorPlan Update(AuthorPlan authorPlan);
+    }
+}
---
+++ Services/Services/AuthorPlanService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Entities;
+using DataAccess.Repositories.AuthorPlanRepository;
+
+namespace Services.Services
+{
+    public class AuthorPlanService
+    {
+        private readonly IAuthorPlanRepository _iAuthorPlanRepository;
+
+        public AuthorPlanService(IAuthorPlanRepository iAuthorPlanRepository)
+        {
+            _iAuthorPlanRepository = iAuthorPlanRepository;
+        }
+
+        public AuthorPlan GetById(Guid id)
+        {
+            return _iAuthorPlanRepository.GetById(id);
+        }
+
+        public IEnumerable<AuthorPlan> GetAll()
+        {
+            return _iAuthorPlanRepository.GetAll();
+        }
+
+        public AuthorPlan Create(AuthorPlan AuthorPlan)
+        {
+            return _iAuthorPlanRepository.Create(AuthorPlan);
+        }
+
+        public AuthorPlan Update(AuthorPlan AuthorPlan)
+        {
+            return _iAuthorPlanRepository.Update(AuthorPlan);
+        }
+
+        public AuthorPlan Delete(Guid id)
+        {
+            return _iAuthorPlanRepository.Delete(id);
+        }
+    }
+}
